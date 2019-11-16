59aba6a1b4a51d66b470b618446f63958cb5b8a5
James Duong <james@simble.io>
2019-11-16 21:20:50 +1100

    #222

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log('hi there');
+import express, { Request, Response } from 'express';
+import { router } from './routes/loginRoutes';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieSession({ keys: ['string'] }));
+app.use(router);
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`
+    <div>
+      <h1>Hi there!</h1>
+    </div>`);
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000');
+});
